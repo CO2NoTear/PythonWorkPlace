6ad57f13f30fe6e11c0f4d9897b8eae63f220fb7
CO2NoTear <740440731@qq.com>
2022-03-03 14:58:31 +0800

    Compeleted GuessingGame.py

---
+++ CreativityHomework/GuessingGame.py
@@ -0,0 +1,16 @@
+import imp
+from random import randint
+
+n = int(randint(1,100))  # destination
+# n = 24
+i = -1
+print('Which number you wanna guess?\n')
+while i != n:
+    i = int(input())
+    if i < 0:
+        print('Error, input a positive number,plz\n')
+        continue
+    if i < n:
+        print('Too Small\n')
+    elif i > n: print('Too big\n')
+print('Congratulations!')
